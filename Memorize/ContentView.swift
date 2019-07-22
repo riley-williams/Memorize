dabca289f84f72992faac68b5be9191f9bd6d899
Riley Williams <williams.riley@me.com>
2019-07-22 13:36:36 -0400

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Riley Williams on 7/22/19.
+//  Copyright © 2019 Riley Williams. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
