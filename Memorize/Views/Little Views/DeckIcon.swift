b2111a7671eb1a026cedfe4477a4646d43d45290
Riley Williams <williams.riley@icloud.com>
2019-08-01 17:52:54 -0400

    Progress bars are inside the icon

--- Memorize/Views/Little Views/DeckIcon.swift
+++ Memorize/Views/Little Views/DeckIcon.swift
@@ -9,73 +9,65 @@
 import SwiftUI
 
 struct DeckIcon: View {
-	var deck:Deck
-	@State var showsDue:Bool = true
-	var width:CGFloat = 70
-	private var radius:CGFloat { get { 10.0*(width/70.0) } }
-	private var separation:CGFloat { get { 2.0*(width/70.0) } }
-	private var offset:CGFloat { get { 3 + width/70 } }
-	private var height:CGFloat { get { width*5.0/7.0 } }
-	
-	var body: some View {
-		ZStack {
-			//			RoundedRectangle(cornerRadius: radius)
-			//				.frame(width: width, height: width*5.0/7.0, alignment: .center)
-			//				.foregroundColor(.white)
-			//				.offset(x: offset, y: -offset)
-			//				.shadow(radius: 2)
-			
-			Image(uiImage:deck.icon)
-				.resizable()
-				.scaledToFill()
-				.frame(width: width*2, height: height*2)
-				.drawingGroup(opaque: true) //fill background of transparent images
+    var deck:Deck
+    @State var showsDue:Bool = true
+    var width:CGFloat = 70
+    private var radius:CGFloat { get { 10.0*(width/70.0) } }
+    private var separation:CGFloat { get { 2.0*(width/70.0) } }
+    private var offset:CGFloat { get { 3 + width/70 } }
+    private var height:CGFloat { get { width*5.0/7.0 } }
+    
+    var body: some View {
+        ZStack {
+            Image(uiImage:deck.icon)
+                .resizable()
+                .scaledToFill()
                 .frame(width: width, height: height)
-                .clipped()
-                .cornerRadius(radius - separation)
-                .overlay(RoundedRectangle(cornerRadius: radius - separation).stroke(Color.white, lineWidth: 2.0))
-                
-				.offset(x: offset, y: -offset)
-                .shadow(radius: 2.0)
-			Image(uiImage:deck.icon)
-				.resizable()
-				.scaledToFill()
-				.frame(width: width, height: height)
-				.drawingGroup(opaque: true) //fill background of transparent images
-				.cornerRadius(radius - separation)
-                .overlay(RoundedRectangle(cornerRadius: radius - separation).stroke(Color.white, lineWidth: 2.0))
+                .drawingGroup(opaque: true) //fill background of transparent images
+                .clipShape(RoundedRectangle(cornerRadius: radius - separation))
                 .shadow(radius: 3.0)
-                
-			if deck.numCardsDue > 0 && showsDue {
-				Text("\(deck.numCardsDue)")
-					.foregroundColor(.white)
-					.padding(.vertical, 0)
+            //.overlay(RoundedRectangle(cornerRadius: radius - separation).stroke(Color.white, lineWidth: 2.0))
+            
+            
+            
+            VStack {
+                Spacer()
+                ProgressBarView(size: .small, progress: deck.mastery, barColor: .yellow, isRounded: false)
+                    
+            }.cornerRadius(radius - separation)
+            
+            
+            if deck.numCardsDue > 0 && showsDue {
+                Text("\(deck.numCardsDue)")
+                    .foregroundColor(.white)
+                    .padding(.vertical, 0)
                     .padding(.horizontal, 5)
-					.background(
+                    .background(
                         RoundedRectangle(cornerRadius: 20.0)
-							.foregroundColor(.red)
-							.frame(minWidth:22))
-					.shadow(radius: 4)
-                    .offset(x: width/2.0 + 4.0, y: -height/2.0-4.0)
-			}
-		}
-	}
+                            .foregroundColor(.red)
+                            .frame(minWidth:22))
+                    .opacity(0.90)
+                    .shadow(radius: 4)
+                    .offset(x: width/2.0, y: -height/2.0)
+            }
+        }.frame(width: width, height: height)
+    }
 }
 
 #if DEBUG
 struct DeckIcon_Previews: PreviewProvider {
-	static var previews: some View {
-		let user = User.testUser(name: "Riley")
-		return Group {
-			DeckIcon(deck:user.decks[0], showsDue: false)
-				.previewLayout(.fixed(width: 200, height: 200))
-			DeckIcon(deck:user.decks[4], width: 70)
-				.previewLayout(.fixed(width: 100, height: 100))
-			DeckIcon(deck:user.decks[1], showsDue: false, width: 119)
-				.previewLayout(.fixed(width: 200, height: 150))
-			DeckIcon(deck:user.decks[3], width: 119)
-				.previewLayout(.fixed(width: 200, height: 150))
-		}
-	}
+    static var previews: some View {
+        let user = User.testUser(name: "Riley")
+        return Group {
+            DeckIcon(deck:user.decks[0], showsDue: false)
+                .previewLayout(.fixed(width: 200, height: 200))
+            DeckIcon(deck:user.decks[4], width: 70)
+                .previewLayout(.fixed(width: 100, height: 100))
+            DeckIcon(deck:user.decks[1], showsDue: false, width: 119)
+                .previewLayout(.fixed(width: 200, height: 150))
+            DeckIcon(deck:user.decks[3], width: 119)
+                .previewLayout(.fixed(width: 200, height: 150))
+        }
+    }
 }
 #endif
--- Memorize/Views/Little Views/ProgressBarView.swift
+++ Memorize/Views/Little Views/ProgressBarView.swift
@@ -9,101 +9,118 @@
 import SwiftUI
 
 enum ProgressBarViewSize {
-	case small
-	case regular
+    case small
+    case regular
 }
 
 struct ProgressBarView: View {
-	var size:ProgressBarViewSize = .regular
-	@State var progress:Float = 0.0
+    var size:ProgressBarViewSize = .regular
+    @State var progress:Float = 0.0
     @State var barColor:Color = .yellow
-	
-	private var height:CGFloat {
-		get {
-			switch size {
-				case .small:
-					return 10
-				case .regular:
-					return 15
-			}
-		}
-		
-	}
-	private var progressOffset:CGFloat {
-		get {
-			switch size {
-				case .small:
-					return 1.5
-				case .regular:
-					return 2
-			}
-		}
-		
-	}
-	private var progressHighlightOffset:CGFloat {
-		get {
-			switch size {
-				case .small:
-					return 2
-				case .regular:
-					return 3
-			}
-		}
-		
-	}
-	
-	var body: some View {
-		return GeometryReader { geometry in
-			
-			ZStack(alignment: .topLeading) {
-				RoundedRectangle(cornerRadius:self.height/2) //base
-					.frame(width: geometry.size.width, height: self.height)
-					.foregroundColor(.gray)
-				
-				RoundedRectangle(cornerRadius: (self.height-self.progressOffset*2)/2) //bar
-					.frame(width: self.interpolate(self.progress, a:self.height-self.progressOffset*2.0, b:geometry.size.width-self.progressOffset*2.0), height: self.height-self.progressOffset*2)
+    @State var isRounded:Bool = true
+    
+    private var height:CGFloat {
+        get {
+            switch size {
+            case .small:
+                return 10
+            case .regular:
+                return 15
+            }
+        }
+        
+    }
+    private var progressOffset:CGFloat {
+        get {
+            switch size {
+            case .small:
+                return 1.5
+            case .regular:
+                return 2
+            }
+        }
+        
+    }
+    private var progressHighlightOffset:CGFloat {
+        get {
+            switch size {
+            case .small:
+                return 2
+            case .regular:
+                return 3
+            }
+        }
+        
+    }
+    
+    var body: some View {
+        return GeometryReader { geometry in
+            
+            ZStack(alignment: .topLeading) {
+                //under
+                RoundedRectangle(cornerRadius:(self.isRounded ? self.height/2 : 0))
+                    .frame(width: geometry.size.width, height: self.height)
+                    .foregroundColor(.gray)
+                
+                //over
+
+                    Rectangle()
+                    .frame(width: self.interpolate(self.progress, a:self.height, b:geometry.size.width), height: self.height)
                     .foregroundColor(self.barColor)
-					.offset(x: self.progressOffset, y: self.progressOffset)
-				
-				
-			}.frame(height:self.height)
-		}.frame(height:self.height)
-			.accessibility(label: Text("\(progress)% complete"))
-	}
-	
-	private func interpolate(_ percent:Float, a:CGFloat, b:CGFloat) -> CGFloat {
-		return a + (b-a)*CGFloat(percent)
-	}
+                
+            }
+            .frame(height:self.height)
+            .cornerRadius(self.isRounded ? self.height / 2 : 0)
+            
+        }.frame(height:self.height)
+            .accessibility(label: Text("\(progress)% complete"))
+    }
+    
+    private func interpolate(_ percent:Float, a:CGFloat, b:CGFloat) -> CGFloat {
+        return a + (b-a)*CGFloat(percent)
+    }
 }
 
 #if DEBUG
 struct ProgressBarView_Previews: PreviewProvider {
-	static var previews: some View {
-		return Group {
-			VStack {
-				ProgressBarView(size:.small)
-					.frame(width:100.0)
-				
-				ProgressBarView(size:.small, progress: 0.5)
-					.frame(width:100.0)
-				
-				ProgressBarView(size:.small, progress: 1.0)
-					.frame(width:100.0)
-				
-			}.previewLayout(.fixed(width: 150, height: 100))
-			
-			VStack {
-				ProgressBarView(size:.regular)
-					.frame(width:100)
-				
-				ProgressBarView(size:.regular, progress: 0.5)
-					.frame(width:150)
-				
-				ProgressBarView(size:.regular, progress: 1.0)
-					.frame(width:200)
-				
-			}.previewLayout(.fixed(width: 250, height: 100))
-		}
-	}
+    static var previews: some View {
+        return Group {
+            VStack {
+                ProgressBarView(size:.small)
+                    .frame(width:100.0)
+                
+                ProgressBarView(size:.small, progress: 0.5)
+                    .frame(width:100.0)
+                
+                ProgressBarView(size:.small, progress: 1.0)
+                    .frame(width:100.0)
+                
+            }.previewLayout(.fixed(width: 150, height: 100))
+            
+            VStack {
+                ProgressBarView(size:.regular)
+                    .frame(width:100)
+                
+                ProgressBarView(size:.regular, progress: 0.5)
+                    .frame(width:150)
+                
+                ProgressBarView(size:.regular, progress: 1.0)
+                    .frame(width:200)
+                
+            }.previewLayout(.fixed(width: 250, height: 100))
+            
+            VStack {
+                ProgressBarView(size:.regular, isRounded: false)
+                    .frame(width:100)
+                
+                ProgressBarView(size:.regular, progress: 0.5, isRounded: false)
+                    .frame(width:150)
+                
+                ProgressBarView(size:.regular, progress: 1.0, isRounded: false)
+                    .frame(width:200)
+                
+            }.previewLayout(.fixed(width: 250, height: 100))
+        }
+    }
 }
 #endif
